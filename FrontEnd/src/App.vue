6bdf7317ccb0fa58cc5225785481df656117e8a5
Arturo Pérez <arjperez31@gmail.com>
2024-05-28 22:43:59 -0400

    Adding frontend and backend

---
+++ FrontEnd/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Listado de posts:</h1>
+        <PostContainer/>
+    </div>  
+</template>
+
+<script setup>
+    import PostContainer from './components/PostContainer.vue'
+</script>
+
+<style scoped>
+    h1 {
+        color: green
+    }
+</style>
+
+
+
---
+++ FrontEnd/src/Javascript/Petitions.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+export const persona = {};
+axios({
+    method:'get',
+    url: 'http://localhost:3000/index',
+}).then((Response)=> Response.data).then(data=> console.log(data));
+
